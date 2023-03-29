f577fa5cc3864c8d51d804343330eef0fb365377
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-03-28 21:40:32 -0400

    1.functions have been updated 2. Deleted the commented code.

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ let productB = {
 
 const starProductBtn = document.getElementById("star-product");
 const fireProductBtn = document.getElementById("fire-product");
-// const soldProducts = document.getElementById("sold-products");
-// const achievements = document.getElementById("achievements");
-// const totalRevenue = document.getElementById("total-revenue");
-// const totalCommission = document.getElementById("total-commission");
 
 const obj = {
   soldProducts: document.getElementById("sold-products"),
@@ -26,7 +22,7 @@ const obj = {
   totalCommission: document.getElementById("total-commission"),
 };
 
-const salesAndIncentivesData = {
+let salesAndIncentivesData = {
   soldProducts: [],
   achievements: [],
   totalRevenue: 0,
@@ -40,58 +36,40 @@ function handleClick() {
     e.preventDefault();
     switch (e.target.dataset.product) {
       case "star":
-        obj["soldProducts"].innerHTML += productA.emoji;
         updateObjProp("soldProducts", productA.emoji);
-        addToObjProp("totalRevenue", productA.revenue);
-        addToObjProp("totalCommission", productA.commission);
+        updateObjProp("totalRevenue", productA.revenue);
+        updateObjProp("totalCommission", productA.commission);
         break;
       case "fire":
-        obj["soldProducts"].innerHTML += productB.emoji;
         updateObjProp("soldProducts", productB.emoji);
-        addToObjProp("totalRevenue", productB.revenue);
-        addToObjProp("totalCommission", productB.commission);
+        updateObjProp("totalRevenue", productB.revenue);
+        updateObjProp("totalCommission", productB.commission);
         break;
       default:
         break;
     }
     updateAchievementsHtml();
-    renderData("totalRevenue", salesAndIncentivesData.totalRevenue);
-    renderData("totalCommission", salesAndIncentivesData.totalCommission);
-    // updateRevenueOrCommissionHtml(
-    //   obj["totalRevenue"],
-    //   salesAndIncentivesData.totalRevenue
-    // );
-    // updateRevenueOrCommissionHtml(
-    //   obj["totalCommission"],
-    //   salesAndIncentivesData.totalCommission
-    // );
     setLocalStorage();
+    renderData();
   });
 }
 
-// what if i pass in property name and the call both objects using the argument passed in to the function
-
 function updateObjProp(property, value) {
-  salesAndIncentivesData[property].push(value);
+  if (Array.isArray(salesAndIncentivesData[property])) {
+    salesAndIncentivesData[property].push(value);
+  } else {
+    salesAndIncentivesData[property] += value;
+  }
 }
 
 function updateAchievementsHtml() {
-  addAndStoreBellIconOnFirstProductSale();
+  addBellIconOnFirstProductSale();
   addCurrencyIconWhenAmountExceedsThreshold();
-  addAndStorePrizeIconOnFifteenthSale();
+  addPrizeIconOnFifteenthSale();
 }
 
-// function updateRevenueOrCommissionHtml(key, income) {
-//   // htmlElement.textContent = `$ ${income}`;
-//   renderData(key, income);
-// }
-
-function addAndStoreBellIconOnFirstProductSale() {
-  if (
-    obj["soldProducts"].innerHTML === productA.emoji ||
-    obj["soldProducts"].innerHTML === productB.emoji
-  ) {
-    obj["achievements"].innerHTML = "🔔";
+function addBellIconOnFirstProductSale() {
+  if (salesAndIncentivesData["soldProducts"].length === 1) {
     updateObjProp("achievements", "🔔");
   }
 }
@@ -100,22 +78,16 @@ function addCurrencyIconWhenAmountExceedsThreshold() {
   const thresholdAmount = 2500;
   const totalSalesRevenue = salesAndIncentivesData.totalRevenue;
   if (totalSalesRevenue >= thresholdAmount) {
-    obj["achievements"].innerHTML += "💰";
     updateObjProp("achievements", "💰");
   }
 }
 
-function addAndStorePrizeIconOnFifteenthSale() {
+function addPrizeIconOnFifteenthSale() {
   const soldProductsMultiplier = 15;
   const salesData = salesAndIncentivesData.totalRevenue;
   if (salesData % soldProductsMultiplier === 0) {
-    obj["achievements"].innerHTML += "🏆";
     updateObjProp("achievements", "🏆");
   }
-}
-
-function addToObjProp(prop, income) {
-  salesAndIncentivesData[prop] += income;
 }
 
 function setLocalStorage() {
@@ -123,51 +95,32 @@ function setLocalStorage() {
 }
 
 function getDataFromLocalStorage() {
-  return JSON.parse(localStorage.getItem("salesData")) || {};
+  return (
+    JSON.parse(localStorage.getItem("salesData")) || {
+      soldProducts: [],
+      achievements: [],
+      totalRevenue: 0,
+      totalCommission: 0,
+    }
+  );
 }
 
 window.addEventListener("load", (e) => {
   e.preventDefault();
-  const salesDataFromLocalStorage = getDataFromLocalStorage();
-  for (const [key, value] of Object.entries(salesDataFromLocalStorage)) {
-    renderData(key, value);
-  }
+  renderData();
 });
 
-// there are two functions which are updating the html, those two function will be replaced by the render function.
-
-function renderData(htmlElement, value) {
-  let data = "";
-  if (Array.isArray(value)) {
-    value.forEach((element) => {
-      data += `<span>${element}</span>`;
-    });
-  } else {
-    data = value;
+function renderData() {
+  salesAndIncentivesData = getDataFromLocalStorage();
+  for (const [key, value] of Object.entries(salesAndIncentivesData)) {
+    let data = "";
+    if (Array.isArray(value)) {
+      value.forEach((element) => {
+        data += `<span>${element}</span>`;
+      });
+    } else {
+      data = `$ ${value}`;
+    }
+    obj[key].innerHTML = data;
   }
-  obj[htmlElement].innerHTML = data;
 }
-
-// Puzzle Pieces:
-// 1. Which functions updates the DOM?
-// ==> 1. UpdatesAchievementsHtml
-// =====> 1. addAndStoreBellIconOnFirstProductSale();
-// =====> 2. addCurrencyIconWhenAmountExceedsThreshold();
-// =====> 3. addAndStorePrizeIconOnFifteenthSale();
-// ==> 2. updateRevenueOrCommissionHtml => REPLACED WITH RENDERDATA FUNCTION
-
-// 2. render function takses in two parameters
-// ==> 1. key for the object property
-// ==> 2. value of the object's key whose proeprty is passed in
-
-// ==> 1. When calling render function
-// =====> 1. Pass object's key
-// =====> 2. soldAndIncentivesData's value according to which key is passed in
-
-// First Puzzle Piece: updateRevenueOrCommissionHtml
-// 1. this function has a variable which holds the salesAndIncentivesData property's value
-// 2, second parameter -> the object's property which holds the html element
-
-// 1. Call renderData function inside this function
-// What parameters does this function needs?
-// ===> 1. key and value
